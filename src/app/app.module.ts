bb6663972b2ad567e2c61f7ee93def1f953428a1
Gerardin <niveadha@gmail.com>
2021-02-11 01:12:00 +0530

    Title case

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
+
 import { AuthorsService } from './authors.service';
 import { CoursesService } from './course.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseComponent } from './course.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { TitleCaseComponent } from './titlecase/titlecase.component';
+import { HomeComponent } from './home/home.component';
+import { TitlePipe } from './titlecase.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseComponent,
-    AuthorsComponent
+    AuthorsComponent,
+    TitleCaseComponent,
+    HomeComponent,
+    TitlePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [CoursesService,AuthorsService],
   bootstrap: [AppComponent]
